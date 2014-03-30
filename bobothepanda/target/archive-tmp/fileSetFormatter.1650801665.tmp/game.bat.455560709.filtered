ee3475b9f0b38a8a212c38cef0980a4c8de2c5b9
Elvira Jonsson <elviraj@student.chalmers.se>
2014-03-30 18:33:02 +0200

    Deleted project

--- bobothepanda/target/archive-tmp/fileSetFormatter.1650801665.tmp/game.bat.455560709.filtered
+++
@@ -1,2 +0,0 @@
-@echo off
-java -jar -Djava.library.path="lib/" "game-0.0.1-SNAPSHOT.jar"
--- bobothepanda/target/game-0.0.1-SNAPSHOT-release/game-0.0.1-SNAPSHOT/game.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-BASE=`dirname $0`
-java -jar -Djava.library.path="$BASE/lib/" "$BASE/game-0.0.1-SNAPSHOT.jar" &
