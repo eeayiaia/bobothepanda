d1e66e6ac58064f86d7c61ed87ddd7cfa50f900b
HTomas <viclars@student.chalmers.se>
2014-04-01 21:55:45 +0200

    Created Texture folder, contains the BoBo image, implemented the image

--- project/bobothepanda/src/main/java/jar/SimpleSlickGame.java
+++ project/bobothepanda/src/main/java/jar/SimpleSlickGame.java
@@ -1,20 +1,25 @@
 package jar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class SimpleSlickGame extends BasicGame {
 
+	private Image boboImage;
+	
 	public SimpleSlickGame(String gamename) {
 		super(gamename);
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
+		boboImage = new Image("Textures/Bobo-03.png");
 	}
 
 	@Override
@@ -22,14 +27,18 @@ public class SimpleSlickGame extends BasicGame {
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-		g.drawString("Howdy!", 10, 10);
+		//g.drawString("Howdy!", 10, 10);
+		boboImage.draw(320, 240);
 	}
 
 	public static void main(String[] args) {
 		try {
 			AppGameContainer appgc;
-			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
+			appgc = new AppGameContainer(new SimpleSlickGame("BoBo The Panda"));
 			appgc.setDisplayMode(640, 480, false);
+			
+			appgc.setShowFPS(true);
+			
 			appgc.start();
 		} catch (SlickException ex) {
 			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
