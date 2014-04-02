73059fe10e6347e2f8768158ed72e5a04e6e5aa9
HTomas <viclars@student.chalmers.se>
2014-04-02 09:25:27 +0200

    Worked on the level class, added render method

--- project/bobothepanda/src/main/java/game/level/Level.java
+++ project/bobothepanda/src/main/java/game/level/Level.java
@@ -1,5 +1,33 @@
 package game.level;
 
+//import java.util.ArrayList;
+import game.character.Character;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
 public class Level {
 
+	private TiledMap map;
+	private Character character;
+	//private ArrayList<Character> character;
+	
+	public Level(String level) throws SlickException{
+		map = new TiledMap("Maps/" + level + "/.tmx","Textures");
+		//character = new ArrayList<Character>();
+	}
+	
+	public void addCharacter(Character c){
+		character = c;
+		//character.add(c);
+	}
+	
+	public void render(){
+		map.render(0,0);
+		/*
+		for(Character c: character){
+			c.render();
+		}
+		*/
+		character.render();
+	}
 }
