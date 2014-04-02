c718ca24080a5a4d66943fc58a6b1141c682af04
Elvira Jonsson <elviraj@student.chalmers.se>
2014-04-02 09:01:35 +0200

    LevelState implemented

--- project/bobothepanda/src/main/java/game/state/LevelState.java
+++ project/bobothepanda/src/main/java/game/state/LevelState.java
@@ -1,5 +1,46 @@
 package game.state;
 
-public class LevelState {
+import game.level.Level;
 
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class LevelState extends BasicGameState {
+	private Level level;
+	private String firstLevel;
+
+	public LevelState(String firstLevel){
+		this.firstLevel = firstLevel;
+	}
+	
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		level = new Level(firstLevel);
+		
+	}
+
+	public void render(GameContainer container, StateBasedGame game, Graphics g)
+			throws SlickException {
+		g.scale(Game.SCALE, Game.SCALE);
+		level.render();
+	}
+
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void keyPressed(int key, char code){
+		if(key == Input.KEY_ESCAPE){
+			System.exit(0);
+		}
+	}
 }
