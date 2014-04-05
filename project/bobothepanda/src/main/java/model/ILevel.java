4d2558b7637ece40410b619e0742cb09cc305f60
Oscar Muhr <oscarmu@student.chalmers.se>
2014-04-05 14:08:31 +0200

    Worked some on the LevelModel

--- project/bobothepanda/src/main/java/model/ILevel.java
+++ project/bobothepanda/src/main/java/model/ILevel.java
@@ -1,5 +1,5 @@
 package model;
 
 public interface ILevel {
-
+	
 }
--- project/bobothepanda/src/main/java/model/LevelModel.java
+++ project/bobothepanda/src/main/java/model/LevelModel.java
@@ -1,5 +1,17 @@
 package model;
 
-public interface LevelModel {
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
+public class LevelModel implements ILevel {
+	TiledMap map;
+	CharacterModel Bobo;
+	Position startPosition = new Position(128, 416);
+	
+	public LevelModel(String file, String folder) throws SlickException{
+		map = new TiledMap(file, folder);
+		Bobo = new CharacterModel(startPosition);
+	}
+	
+	
 }
