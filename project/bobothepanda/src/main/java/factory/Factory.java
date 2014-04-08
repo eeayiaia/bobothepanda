6d5297cbfe8d5d2390118c43e8bf0137e4ff8205
Elvira Jonsson <elviraj@student.chalmers.se>
2014-04-08 15:57:42 +0200

    Started working on factory

--- project/bobothepanda/src/main/java/factory/Factory.java
+++ project/bobothepanda/src/main/java/factory/Factory.java
@@ -1,5 +1,20 @@
 package factory;
 
+import Map.IMapHandler;
+import Map.MapHandler;
+import model.CharacterModel;
+import model.LevelModel;
+
 public class Factory {
 
+	private CharacterModel playerCharacter;
+	private LevelModel level;
+	private IMapHandler map;
+	
+	
+	public Factory(String levelName){
+		map = new MapHandler();
+		playerCharacter = new CharacterModel(map.getCharacterStartPosition());
+		level = new LevelModel(map.getMapObjectList(), playerCharacter);
+	}
 }
--- project/bobothepanda/src/main/java/model/LevelModel.java
+++ project/bobothepanda/src/main/java/model/LevelModel.java
@@ -1,17 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
-
 import utilities.Position;
 
 public class LevelModel implements ILevel {
 	TiledMap map;
-	CharacterModel Bobo;
-	Position startPosition = new Position(128, 416);
+	CharacterModel playerCharacter;
 	
-	public LevelModel(String file, String folder) throws SlickException{
-		map = new TiledMap(file, folder);
+	public LevelModel(ArrayList<IMapObject> objectList, CharacterModel playerCharacter) throws SlickException{
+		this.playerCharacter = playerCharacter;
 	}
 	
 	
