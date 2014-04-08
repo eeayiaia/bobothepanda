f0a513c9326fb02d48a7f7da279b3846acc39259
Oscar Muhr <oscarmu@student.chalmers.se>
2014-04-08 13:39:06 +0200

    Implemented size class

--- project/bobothepanda/src/main/java/utilities/Size.java
+++ project/bobothepanda/src/main/java/utilities/Size.java
@@ -1,5 +1,23 @@
 package utilities;
 
 public class Size {
-
+	public float height;
+	public float width;
+	
+	public Size(float width, float height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public float getWidth() {
+		return width;
+	}
+	
+	public float getHeight() {
+		return height;
+	}
+	
+	public float getArea() {
+		return width*height;
+	}
 }
