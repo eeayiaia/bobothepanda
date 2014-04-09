1006b21431f3f2153ae7cbbf02a0117a15703981
Victor Larsson <viclars@student.chalmers.se>
2014-04-09 13:57:22 +0200

    Created LevelView class

---
+++ project/bobothepanda/src/main/java/view/LevelView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class LevelView implements PropertyChangeListener{
+
+	public void propertyChange(PropertyChangeEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
