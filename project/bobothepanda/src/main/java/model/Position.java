540ed8f190f4a503deb280e690305e464d342d76
Sebastian Sandberg <seb.sandberg@gmail.com>
2014-05-02 12:08:42 +0200

    fixed return statement in Position.hashcode

--- project/bobothepanda/src/main/java/model/Position.java
+++ project/bobothepanda/src/main/java/model/Position.java
@@ -42,9 +42,7 @@ public class Position {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + Float.floatToIntBits(x);
-		result = prime * result + Float.floatToIntBits(y);
-		return result;
+		return prime * (prime * result + Float.floatToIntBits(x)) + Float.floatToIntBits(y);
 	}
 
 	@Override
