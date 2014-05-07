9789610f88de39ada709488ce35606f03eb973b2
romenor <seb.sandberg@gmail.com>
2014-05-07 21:58:31 +0200

    fixed FindBugs bugs in CollisionTest

--- project/bobothepanda/src/test/java/bobothepanda/CollisionTest.java
+++ project/bobothepanda/src/test/java/bobothepanda/CollisionTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import model.Collision;
 import model.Character;
 import model.MapObject;
-import model.ObjectType;
 import model.Position;
 import model.Size;
 import model.Terrain;
@@ -19,24 +18,31 @@ import org.junit.Test;
 public class CollisionTest extends Assert{
 	private Collision collision;
 	private Character character;
-	private MapObject terrain;
 	private final List <MapObject> list = new ArrayList <MapObject>();
+
 
 	@Before
 	public void setUp() throws Exception {
-		terrain = new Terrain(new Position(1f,1f), new Size(5f,5f));
-		list.add(terrain);
 		collision = new Collision(list);
 		character = new Character(new Position(3f,3f), collision);
 	}
 
 	@Test
-	public void testCollidedWith() {
+	public void testCollidedWith(){
+		final MapObject terrain = new Terrain(new Position(1f,1f), new Size(5f,5f));
+		list.add(terrain);
 		assertTrue(collision.collidedWith(character.getHitbox()).equals(terrain.getHitbox()));
+	}
+	
+	@Test
+	public void testCollidedWithNull() {
+		assertNull(collision.collidedWith(null));
 	}
 
 	@Test
 	public void testGetObjectType() {
+		final MapObject terrain = new Terrain(new Position(1f,1f), new Size(5f,5f));
+		list.add(terrain);
 		collision.collidedWith(character.getHitbox());
 		assertSame(terrain, collision.getObjectType());
 	}
@@ -47,6 +53,8 @@ public class CollisionTest extends Assert{
 
 	@Test
 	public void testGetObjectPosition() {
+		final MapObject terrain = new Terrain(new Position(1f,1f), new Size(5f,5f));
+		list.add(terrain);
 		collision.collidedWith(character.getHitbox());
 		assertTrue(collision.getObjectPosition().equals(new Position(1f,1f)));
 	}
