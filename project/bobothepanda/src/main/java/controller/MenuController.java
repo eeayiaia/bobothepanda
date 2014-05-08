70274f46116eb947a1f3dd944c34b548960cd5fc
Elvira Jonsson <elviraj@student.chalmers.se>
2014-05-08 17:14:55 +0200

    Renders menu

--- project/bobothepanda/src/main/java/controller/MenuController.java
+++ project/bobothepanda/src/main/java/controller/MenuController.java
@@ -7,41 +7,46 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import main.MainClass;
 import model.Position;
 import model.menu.Menu;
 import utilities.MenuMapHandler;
 
 public class MenuController extends BasicGameState{
 	private Menu menu;
+	private MenuMapHandler menuMapHandler;
+	private GameContainer container;
 	
-	public MenuController(Menu menu){
-		this.menu = menu;
+	public MenuController(){
+		super();
+		menuMapHandler = new MenuMapHandler();
+		menu = new Menu(menuMapHandler.getMenuItemList());
 	}
 	public void handleInput(Input i, int delta){
-		final Position cursorPos = new Position(i.getAbsoluteMouseX(),i.getAbsoluteMouseY());
-		if(i.isMousePressed(delta)){
+		final Position cursorPos = new Position((float)i.getAbsoluteMouseX(),(float)i.getAbsoluteMouseY());
+		if(i.isMousePressed(Input.MOUSE_LEFT_BUTTON)){
 			menu.mouseClicked(cursorPos);
 		}
 	}
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		this.container = container;
+
 		
 	}
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		
+		g.scale(MainClass.SCALE, MainClass.SCALE);
+		menuMapHandler.renderMap();		
 	}
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		handleInput(container.getInput(),delta);
 		
 	}
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 }
