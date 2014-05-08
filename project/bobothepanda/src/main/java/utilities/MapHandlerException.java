66e5d5f33e95b1a7ab119ba60cc85e4b1ed7020b
Elvira Jonsson <elviraj@student.chalmers.se>
2014-05-08 10:44:39 +0200

    Added maphandlerexception

---
+++ project/bobothepanda/src/main/java/utilities/MapHandlerException.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class MapHandlerException extends RuntimeException {
+	public MapHandlerException(){
+		super();
+	}
+	public MapHandlerException(String message){
+		super(message);
+	}
+}
