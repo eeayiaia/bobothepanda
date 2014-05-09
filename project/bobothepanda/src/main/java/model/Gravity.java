89783afa81cf109ddc41a9574dc3f11a1908ad33
Sebastian Sandberg <seb.sandberg@gmail.com>
2014-05-09 11:57:20 +0200

    Added a method in gravity to get the new velocity

--- project/bobothepanda/src/main/java/model/Gravity.java
+++ project/bobothepanda/src/main/java/model/Gravity.java
@@ -10,11 +10,30 @@ public class Gravity {
 	public int getGravity(){
 		return this.GRAVITY;
 	}
-	public float applyGravity(int YPosition, float YVelocity, int delta){
+	
+	/**
+	 * Calculates the new yVelocity of the object
+	 * 
+	 * @param YVelocity The current yVelocity of the object
+	 * @param delta The time difference
+	 * @return The new Velocity of the object
+	 */
+	public float getNewVelocity(float yVelocity, int delta){
 		//velocity formula
-		final float DY = YVelocity + this.getGravity() * delta;
+		return yVelocity + this.getGravity() * delta;
+	}
+	
+	/**
+	 * Calculates the new yPosition of the object
+	 * 
+	 * @param yPosition The current yPosition of the object
+	 * @param yVelocity	The current yVelocity of the object
+	 * @param delta	The difference in time
+	 * @return The new yPosition of the object
+	 */
+	public float getNewPosition(int yPosition, float yVelocity, int delta){
 		//position formula
-		return (float) (YPosition + DY * delta + 0.5 * this.getGravity() * delta * delta);
+		return (float) (yPosition + yVelocity * delta + 0.5 * this.getGravity() * delta * delta);
 		
 	}
 }
