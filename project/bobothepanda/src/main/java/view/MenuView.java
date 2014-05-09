36f61cd468106cf34400bfa6dca35f7cc2bb2e97
Elvira Jonsson <elviraj@student.chalmers.se>
2014-05-09 12:31:01 +0200

    Created constructor in menuview

--- project/bobothepanda/src/main/java/view/MenuView.java
+++ project/bobothepanda/src/main/java/view/MenuView.java
@@ -1,5 +1,31 @@
 package view;
 
-public class MenuView {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+public class MenuView implements PropertyChangeListener{
+	private final Image startButton;
+	private final Image quitButton;
+	private final Animation characterAnimation;
+	private final static String SPRITE_RIGHT_IMAGE = "/data/Bobo-01.png";
+
+	public MenuView() throws SlickException{
+		final Image [] boboRightAnimation = {new Image(SPRITE_RIGHT_IMAGE), new Image("/data/BoboRightLeg-01.png"),
+                new Image(SPRITE_RIGHT_IMAGE), new Image("/data/BoboLeftLeg-01.png")};
+		startButton = new Image("data/img/startButton.png");
+		quitButton = new Image("data/img/quitButton.png");
+		characterAnimation = new Animation(boboRightAnimation, 125);
+
+		
+	}
+
+	public void propertyChange(PropertyChangeEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
