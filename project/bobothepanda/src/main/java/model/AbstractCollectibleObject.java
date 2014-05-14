e77c394a5e41a8ccef90ea7264d5c88b79107384
Oscar Muhr <oscarmu@student.chalmers.se>
2014-05-14 14:08:13 +0200

    Fixed inheritance in collectibles

--- project/bobothepanda/src/main/java/model/AbstractCollectibleObject.java
+++ project/bobothepanda/src/main/java/model/AbstractCollectibleObject.java
@@ -1,11 +1,6 @@
 package model;
 
-public abstract class AbstractCollectibleObject extends AbstractFixedObject implements IVisitable {
-
-	public AbstractCollectibleObject(Position position, Size size) {
-		super(position, size);
-		// TODO Auto-generated constructor stub
-	}
+public abstract class AbstractCollectibleObject implements IVisitable {
 
 	public void accept(IVisitor visitor) {
 		// TODO Auto-generated method stub
--- project/bobothepanda/src/main/java/model/Key.java
+++ project/bobothepanda/src/main/java/model/Key.java
@@ -3,10 +3,6 @@ package model;
 @SuppressWarnings("PMD")
 public class Key extends AbstractCollectibleObject {
 
-	public Key(Position position, Size size) {
-		super(position, size);
-	}
-
 	public void accept(IVisitor visitor) {
 		// TODO Auto-generated method stub
 		
