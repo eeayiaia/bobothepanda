220fc5cf0c2e44a3ba5d64e3aab246049b41001f
Sebastian Sandberg <seb.sandberg@gmail.com>
2014-05-14 14:10:38 +0200

    added extensions and implementions in abstract moving object

--- project/bobothepanda/src/main/java/model/AbstractMovingObject.java
+++ project/bobothepanda/src/main/java/model/AbstractMovingObject.java
@@ -1,5 +1,20 @@
 package model;
 
-public class AbstractMovingObject implements IVisitor{
+public class AbstractMovingObject extends AbstractMapObject implements IVisitor, IVisitable{
+
+	public AbstractMovingObject(Position position, Size size) {
+		super(position, size);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void visit(IVisitable i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void accept(IVisitor visitor) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
