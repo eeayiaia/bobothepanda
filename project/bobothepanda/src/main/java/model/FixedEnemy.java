7176095dd8e49c8ecd6eb6341933dab34fe56e41
Victor Larsson <viclars@student.chalmers.se>
2014-05-14 17:11:10 +0200

    Cleaned FixedEnemy

--- project/bobothepanda/src/main/java/model/FixedEnemy.java
+++ project/bobothepanda/src/main/java/model/FixedEnemy.java
@@ -5,12 +5,7 @@ public class FixedEnemy extends AbstractFixedObject {
 	public FixedEnemy(Position position, Size size) {
 		super(position, size);
 	}
-	/**
-	 * Kill the character on collision
-	 */
-	public void doCollision(Character character) {
-		character.die();
-	}
+	
 	public void accept(IVisitor visitor) {
 		visitor.visit(this);
 	}
