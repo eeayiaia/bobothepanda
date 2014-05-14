3da3433178da88775f53b54078610be1f39da10e
Oscar Muhr <oscarmu@student.chalmers.se>
2014-05-14 13:25:40 +0200

    removed unnecessary variables in AbstractFixedObject

--- project/bobothepanda/src/main/java/model/AbstractFixedObject.java
+++ project/bobothepanda/src/main/java/model/AbstractFixedObject.java
@@ -10,16 +10,10 @@ public abstract class AbstractFixedObject implements IVisitable {
 	
 	private final Position position;
 	private final Size size;
-	private final Rectangle hitbox;
-//	private final double TOP_LEFT_ANGLE;
 	
 	public AbstractFixedObject(Position position, Size size) {
 		this.position = position;
 		this.size = size;
-		hitbox = new Rectangle((int)Math.round(position.getX()), (int)Math.round(position.getY()), 
-								(int)Math.round(size.getWidth()), (int)Math.round(size.getHeight()));
-//		TOP_LEFT_ANGLE = calculateTopLeftToMidAngle(position, size);
-//		System.out.println(TOP_LEFT_ANGLE);
 	}
 
 	public Position getPosition() {
@@ -31,7 +25,8 @@ public abstract class AbstractFixedObject implements IVisitable {
 	}
 	
 	public Rectangle getHitbox() {
-		return new Rectangle(hitbox);
+		return new Rectangle((int)Math.round(position.getX()), (int)Math.round(position.getY()), 
+				(int)Math.round(size.getWidth()), (int)Math.round(size.getHeight()));
 	}
 	
 	public void setX(float xValue) {
@@ -46,9 +41,4 @@ public abstract class AbstractFixedObject implements IVisitable {
 		setX(position.getX());
 		setY(position.getY());
 	}
-	
-//	private double calculateTopLeftToMidAngle(Position position, Size size) {
-//		Position centerPosition = new Position(position.getX() + size.getWidth()/2, position.getY() + size.getHeight()/2);
-//		return Math.atan((position.getY() - centerPosition.getY())/(position.getX() - centerPosition.getX())) + Math.PI;
-//	}
 }
