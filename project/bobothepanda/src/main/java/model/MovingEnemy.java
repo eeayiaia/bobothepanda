4a1d0426bc73c459d6396ba01d9acb9187587b26
Victor Larsson <viclars@student.chalmers.se>
2014-05-14 16:23:28 +0200

    Cleaned MovingEnemy

--- project/bobothepanda/src/main/java/model/MovingEnemy.java
+++ project/bobothepanda/src/main/java/model/MovingEnemy.java
@@ -2,33 +2,17 @@ package model;
 
 public class MovingEnemy extends AbstractMovingObject {
 	
-	private int xDirection = 1;
+	private int xDirection;
 	private boolean dead;
 
 	public MovingEnemy(Position position, Size size) {
 		super(position, size);
 		dead = false;
-		// TODO Auto-generated constructor stub
-	}
-	
-	/**
-	 * If the character comes from above, object will remove itself,
-	 * else character dies.
-	 */
-	public void doCollision(Character character){
-		//if character comes from above
-		if(this.getPosition().getY() > character.getPosition().getY()){
-			dead = true;
-			//TODO remove self
-			//property change or update call? Or anything at all? 
-			//Should level.update always iterate the list for enemy.isdead() == remove from list?
-		}else{
-			character.die();
-		}
+		xDirection = 1;
 	}
 	
 	public void setNewX(int delta){
-		final float nextPositionX = getPosition().getX() * this.xDirection * delta;
+		final float nextPositionX = getPosition().getX() - xDirection * delta;
 		setX(nextPositionX);
 	}
 	
@@ -36,15 +20,23 @@ public class MovingEnemy extends AbstractMovingObject {
 	 * reverses the xDirection
 	 */
 	private void setReverseXDirection(){
-		this.xDirection *= -1;
+		xDirection *= -1;
 	}
 	
+	/**
+	 * Returns the current direction the enemy is moving, in the x-axis
+	 * @return -1 = moving left, 1 = moving left
+	 */
 	public int getXDirection(){
-		return this.xDirection;
+		return xDirection;
 	}
 	
+	/**
+	 * Returns the current status of the enemy
+	 * @return true if dead, false if alive 
+	 */
 	public boolean isDead(){
-		return this.dead;
+		return dead;
 	}
 	
 	public void visit(MovingEnemy movingEnemy){
@@ -56,6 +48,7 @@ public class MovingEnemy extends AbstractMovingObject {
 	}
 	
 	public void visit(Character character){
+		//If the character comes from above, object will remove itself
 		if(getPosition().getY() > character.getPosition().getY()){
 			dead = true;
 			//TODO remove self 
