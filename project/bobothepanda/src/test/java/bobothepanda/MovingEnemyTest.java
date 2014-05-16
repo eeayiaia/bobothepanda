bfa82e68fe9f7fd971988c13e2709de044f2e35c
Sebastian Sandberg <seb.sandberg@gmail.com>
2014-05-16 14:19:41 +0200

    Added tests for MovingEnemeny

---
+++ project/bobothepanda/src/test/java/bobothepanda/MovingEnemyTest.java
@@ -0,0 +1,36 @@
+package bobothepanda;
+
+import model.MovingEnemy;
+import model.Position;
+import model.Size;
+import model.Character;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MovingEnemyTest extends Assert {
+	
+	MovingEnemy movingEnemy;
+	float velocity;
+	
+	@Before
+	public void setUp(){
+		movingEnemy = new MovingEnemy(new Position(1f,1f), new Size(10,10));
+	}
+	
+	
+	@Test
+	public void testVisitCharacterFromAbove(){
+		Character character = new Character(new Position(0f,0f), new Size(11f,11f));
+		movingEnemy.visit(character);
+		assertTrue(movingEnemy.isDead());
+	}
+	
+	@Test
+	public void testVisitCharacterFromBelow(){
+		Character character = new Character(new Position(10f,10f), new Size(11f,11f));
+		movingEnemy.visit(character);
+		assertFalse(movingEnemy.isDead());
+	}
+}
