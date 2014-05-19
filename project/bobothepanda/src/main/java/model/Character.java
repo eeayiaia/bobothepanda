8580257067a77f103050fff054f0b1199fc82140
Victor Larsson <viclars@student.chalmers.se>
2014-05-19 10:54:10 +0200

    removed comments in character

--- project/bobothepanda/src/main/java/model/Character.java
+++ project/bobothepanda/src/main/java/model/Character.java
@@ -35,15 +35,7 @@ public class Character extends AbstractMovingObject{
 		MOVING_RIGHT,
 		MOVING_LEFT;
 	}
-	
-	/**
-	 * @return The hitbox surrounding the character.
-	 */
-//	public Rectangle getHitbox() {
-//		return new Rectangle((int)Math.round(position.getX()),(int)Math.round(position.getY()),
-//				(int)Math.round(size.getWidth()), (int)Math.round(size.getHeight()));
-//	}
-	
+
 	/**
 	 * Get whether or not the key has been picked up
 	 */
@@ -154,38 +146,13 @@ public class Character extends AbstractMovingObject{
 	}
 	
 	/**
-	 * Checks if the character is on the ground
-	 * @return true if on ground level.
-	 */
-//	public boolean onGround(){
-//		final Position nextPosition = new Position(position.getX(),position.getY()+yVelocity);
-//		final Rectangle collisionHitbox = collision.collidedWith(new Rectangle((int)Math.round(nextPosition.getX()), 
-//				(int)Math.round(nextPosition.getY()), (int)size.getWidth(), (int)size.getHeight()));
-//		// if there is a collision the position remains the same and bobo stands still
-//		if(collisionHitbox != null && (collision.getObjectType().getClass().equals(BlockingObject.class) || collision.getObjectType().getClass() == Door.class)){
-//			return true;
-//		} else{
-//			return false;
-//		}		
-//	}
-	
-	/**
 	 * Applies gravity to the character if it isn't touching the ground.
 	 * @param delta 1000millis divided by the frame rate.
 	 */
 	public void applyGravity(int delta){
 		//Change velocity due to gravity
-		yVelocity = gravity.getNewVelocity(yVelocity, delta);
-		
+		yVelocity = gravity.getNewVelocity(yVelocity, delta);	
 		getPosition().setY(gravity.getNewYPosition(getPosition().getY(), yVelocity, delta));
-//		Position nextPosition;
-//		// Set next Y due to gravity
-////		float yAndYVelocity = gravity.getNewYPosition(position.getY(), yVelocity, delta);/*position.getY() + yVelocity;*/ 
-////		if(yAndYVelocity >= 0){
-////			nextPosition = new Position(position.getX(),yAndYVelocity);
-////		}else{
-////			nextPosition = new Position(position.getX(), position.getY());
-////		}	
 	}
 	
 	public void die() {
@@ -256,111 +223,4 @@ public class Character extends AbstractMovingObject{
 	public void accept(IVisitor visitor) {
 		visitor.visit(this);
 	}
-
-//	public void setNewX(int delta) {
-//		Rectangle collisionHitbox;
-//		Position nextPosition;
-//		
-//		
-//		if(characterState==CharacterState.MOVING_RIGHT) {
-//			nextPosition = new Position(position.getX()+XVELOCITY*delta, position.getY());
-//		} else {
-//			float xMinusVelocity = position.getX()-XVELOCITY*delta;
-//			if(xMinusVelocity>= 0){
-//				nextPosition = new Position(xMinusVelocity, position.getY());
-//			}else{
-//				nextPosition = new Position(position.getX(), position.getY());
-//			}
-//		}
-//		float nextPositionXValue = nextPosition.getX();
-//		collisionHitbox = collision.collidedWith(new Rectangle((int)Math.round(nextPositionXValue), 
-//				(int)Math.round(nextPosition.getY()), WIDTH, HEIGHT));
-//		float collisionHitboxXValue = 0;
-//		if(collisionHitbox != null){
-//			collisionHitboxXValue = (float) collisionHitbox.getX();
-//		}
-//		if(collisionHitbox != null) {	
-//			if(collision.getObjectType().getClass() == BlockingObject.class || collision.getObjectType().getClass() == Door.class || collision.getObjectType().getClass() == Key.class) {
-//				if(collision.getObjectType().getClass() == Key.class){
-//					keyPickedUp = true;
-//					//possible to go right through the key
-//					position.setX(nextPositionXValue);
-//				}else{
-//					//should be ObjectType.OPEN_DOOR
-//					if(collision.getObjectType().getClass() == Door.class && keyPickedUp){
-//						pcs.firePropertyChange("loadLevel", null, null);
-//					}
-//					
-//					if(characterState==CharacterState.MOVING_RIGHT) {
-//						if((nextPositionXValue + WIDTH) >= collisionHitboxXValue) {
-//							position.setX(collisionHitboxXValue - WIDTH);
-//						}
-//					}else if(characterState==CharacterState.MOVING_LEFT) {
-//						double newX = collisionHitboxXValue + collisionHitbox.getWidth();
-//						if(nextPositionXValue <= newX) {
-//							position.setX((float)newX);
-//						}
-//					}
-//				}
-//			} else if(collision.getObjectType().getClass() == LethalEnemy.class) {
-//				pcs.firePropertyChange("reloadLevel", null, null);
-//			}
-//		}
-//		else {
-//			position.setX(nextPositionXValue);
-//		}
-//	}
-//	/**
-//	 * Sets the next y value if the next position is valid
-//	 * @param delta
-//	 */
-//	public void setNewY(int delta){
-//		Rectangle collisionHitbox;
-//		//Change velocity due to gravity
-//		yVelocity = gravity.getNewVelocity(yVelocity, delta);
-//		Position nextPosition;
-//		// Set next Y due to gravity
-//		float yAndYVelocity = gravity.getNewYPosition(position.getY(), yVelocity, delta);/*position.getY() + yVelocity;*/ 
-//		if(yAndYVelocity >= 0){
-//			nextPosition = new Position(position.getX(),yAndYVelocity);
-//		}else{
-//			nextPosition = new Position(position.getX(), position.getY());
-//		}
-//		
-//		collisionHitbox = collision.collidedWith(new Rectangle((int)Math.round(nextPosition.getX()), 
-//				(int)Math.round(nextPosition.getY()), WIDTH, HEIGHT));
-//		// if there is a collision the position remains the same and bobo stands still
-//		if(collisionHitbox != null){
-//			if(collision.getObjectType().getClass() == Key.class){
-//				keyPickedUp = true;
-//				//possible to go right through the key
-//				position.setY(nextPosition.getY());
-//			}
-//			if(collision.getObjectType().getClass() == BlockingObject.class || collision.getObjectType().getClass() == Door.class) {
-//				if(yVelocity < 0) {
-//					position.setY((float)(collisionHitbox.getY() + collisionHitbox.getHeight()));
-//					yVelocity = 0;
-//				} else if(yVelocity > 0) {
-//					position.setY((float)(collisionHitbox.getY() - HEIGHT));
-//				}
-//			} else if(collision.getObjectType().getClass() == LethalEnemy.class) {
-//				pcs.firePropertyChange("reloadLevel", null, null);
-//			}
-//		} else {
-//			position.setY(nextPosition.getY());
-//		}
-//	}
-	/**
-	 * Changes the velocity. Makes sure that the velocity does not exceed a certain amount.
-	 * @param delta
-	 */
-//	private void setYVelocity(int delta){
-//		float gravity = 0.05f;
-//		Float nextYVelocity = yVelocity + gravity * delta;
-//		if(nextYVelocity > 15f){
-//			yVelocity = 15f;
-//		}else{
-//			yVelocity = nextYVelocity;
-//		}
-//	}
 }
