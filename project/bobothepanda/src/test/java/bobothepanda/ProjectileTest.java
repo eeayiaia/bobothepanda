c0b3fb1a603372ee87b1e4c5332d8f62c35a6a31
Oscar Muhr <oscarmu@student.chalmers.se>
2014-05-25 14:31:47 +0200

    Added tests for visit methods in projectile

--- project/bobothepanda/src/test/java/bobothepanda/ProjectileTest.java
+++ project/bobothepanda/src/test/java/bobothepanda/ProjectileTest.java
@@ -3,9 +3,13 @@ package bobothepanda;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import model.Door;
+import model.FixedEnemy;
 import model.Projectile;
+import model.Character;
 import model.Position;
 import model.Size;
+import model.Terrain;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -54,5 +58,33 @@ public class ProjectileTest extends Assert{
 		eventReceived = false;
 		projectile.render();
 		assertTrue(eventReceived);
+	}
+	
+	@Test
+	public void testVisitCharacter() {
+		projectile.setX(25f);
+		projectile.visit(new Character(position, size));
+		assertEquals(position, projectile.getPosition());
+	}
+	
+	@Test
+	public void testVisitTerrain() {
+		projectile.setX(25f);
+		projectile.visit(new Terrain(position, size));
+		assertEquals(position, projectile.getPosition());
+	}
+	
+	@Test
+	public void testVisitFixedEnemy() {
+		projectile.setX(25f);
+		projectile.visit(new FixedEnemy(position, size));
+		assertEquals(position, projectile.getPosition());
+	}
+	
+	@Test
+	public void testVisitDoor() {
+		projectile.setX(25f);
+		projectile.visit(new Door(position, size));
+		assertEquals(position, projectile.getPosition());
 	}
 }
