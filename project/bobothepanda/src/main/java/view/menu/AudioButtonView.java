efef3ae377adeaba7fc59d5abb3c0778e2ca95c7
Elvira Jonsson <elviraj@student.chalmers.se>
2014-05-25 11:49:50 +0200

    Implemented constructor in AudioButtonView

--- project/bobothepanda/src/main/java/view/menu/AudioButtonView.java
+++ project/bobothepanda/src/main/java/view/menu/AudioButtonView.java
@@ -3,10 +3,25 @@ package view.menu;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class AudioButtonView implements PropertyChangeListener{
 	private final static String LOCATION = "data/img/Buttons/";
+	private final Image audioOnDefault;
+	private final Image audioOnHover;
+	private final Image audioOnPressed;
+	private final Image audioOffDefault;
+	private final Image audioOffHover;
+	private final Image audioOffPressed;
 	
-	public AudioButtonView(){
+	public AudioButtonView() throws SlickException {
+		audioOnDefault = new Image(LOCATION + "audioOn.png");
+		audioOnHover = new Image(LOCATION + "hoverAudioOn.png");
+		audioOnPressed = new Image(LOCATION + "pressedAudioOn.png");
+		audioOffDefault = new Image(LOCATION + "audioOff.png");
+		audioOffHover = new Image(LOCATION + "hoverAudioOff.png");
+		audioOffPressed = new Image(LOCATION + "pressedAudioOff.png");
 		
 	}
 
