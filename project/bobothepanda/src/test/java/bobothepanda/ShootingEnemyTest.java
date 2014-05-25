c0af0da7631a1e50ed8511e92c53ea967b8ea67f
Sebastian Sandberg <seb.sandberg@gmail.com>
2014-05-25 14:04:18 +0200

    added tests in ShootingEnemyTest

--- project/bobothepanda/src/test/java/bobothepanda/ShootingEnemyTest.java
+++ project/bobothepanda/src/test/java/bobothepanda/ShootingEnemyTest.java
@@ -1,16 +1,21 @@
 package bobothepanda;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import model.Position;
 import model.ShootingEnemy;
 import model.Size;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class ShootingEnemyTest extends Assert {
 	private ShootingEnemy shootingEnemy;
 	private Size size;
 	private Position position;
+	private boolean eventReceived;
 	
 	@Before
 	public void setUp() {
@@ -19,6 +24,33 @@ public class ShootingEnemyTest extends Assert {
 		shootingEnemy = new ShootingEnemy(position, size);
 	}
 	
-//	@Test
-//	public void 
+	@Test
+	public void testFireProjectile(){
+		shootingEnemy.addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent evt) {
+			    if("ADD_PROJECTILE".equals(evt.getPropertyName())) {
+			    	eventReceived = true;
+			    }	
+		   }
+		});
+		eventReceived = false;
+		shootingEnemy.fireProjectile();
+		assertTrue(eventReceived);
+	}
+	
+	@Test
+	public void testRender(){
+		shootingEnemy.addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent evt) {
+			    if("ENEMY_RIGHT".equals(evt.getPropertyName())) {
+			    	eventReceived = true;
+			    }	
+		   }
+		});
+		eventReceived = false;
+		shootingEnemy.render();
+		assertTrue(eventReceived);
+	}
+	
+	
 }
